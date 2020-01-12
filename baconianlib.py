2121750966abc1c844ee38cc241ff6e11d9b6eff
Venom-Py <45040352+enomarozi@users.noreply.github.com>
2020-01-12 22:49:49 +0700

    Add files via upload

---
+++ baconianlib.py
@@ -0,0 +1,11 @@
+baconianIJUV = {"A":"AAAAA","B":"AAAAB","C":"AAABA","D":"AAABB","E":"AABAA","F":"AABAB"
+            ,"G":"AABBA","H":"AABBB","I":"ABAAA","J":"ABAAA","K":"ABAAB","L":"ABABA"
+            ,"M":"ABABB","N":"ABBAA","O":"ABBAB","P":"ABBBA","Q":"ABBBB","R":"BAAAA"
+            ,"S":"BAAAB","T":"BAABA","U":"BAABB","V":"BAABB","W":"BABAA","X":"BABAB"
+            ,"Y":"BABBA","Z":"BABBB"}
+baconianDistric = {"A":"AAAAA","B":"AAAAB","C":"AAABA","D":"AAABB","E":"AABAA","F":"AABAB"
+                   ,"G":"AABBA","H":"AABBB","I":"ABAAA","J":"ABAAB","K":"ABABA","L":"ABABB"
+                   ,"M":"ABBAA","N":"ABBAB","O":"ABBBA","P":"ABBBB","Q":"BAAAA","R":"BAAAB"
+                   ,"S":"BAABA","T":"BAABB","U":"BABAA","V":"BABAB","W":"BABBA","X":"BABBB"
+                   ,"Y":"BBAAA","Z":"BBAAB"}
+
